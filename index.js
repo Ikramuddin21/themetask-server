e9cb0a9d1dc56d8b332d1ac16557e480dd372054
Ikramuddin21 <shaonakhand2122@gmail.com>
2024-05-19 23:56:52 +0600

    index js

--- index.js
+++ index.js
@@ -2,17 +2,11 @@ const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
-const corsConfig = {
-  origin: "*",
-  credential: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-};
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.options("", cors(corsConfig));
-app.use(cors(corsConfig));
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ilfiw.mongodb.net/?retryWrites=true&w=majority`;
@@ -33,7 +27,7 @@ function getApi(path, collection) {
 
 async function run() {
   try {
-    client.connect();
+    await client.connect();
     const database = client.db("themetask_theme");
     const carouselCollection = database.collection("carousel");
     const themetaskCollection = database.collection("themetask");
